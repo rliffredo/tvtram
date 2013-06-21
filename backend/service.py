4c05519e61163227f65d29218cfe90b2220251a6
Roberto Liffredo <rliffredo@gmail.com>
2013-06-21 11:33:34 +0200

    Added periodic schedule update

--- backend/service.py
+++ backend/service.py
@@ -1,5 +1,9 @@
 import bottle
 import scraper
+import threading
+import time
+
+schedules = None
 
 @bottle.get('/hello')
 def get_hello():
@@ -7,12 +11,32 @@ def get_hello():
     
 @bottle.get('/stops')
 def get_stops():
+    global schedules
+    if schedules is None:
+        bottle.abort(text='Initialization in progress')
     return schedules
 
-schedules = scraper.get_schedules([
-    'Rondo Matecznego',
-    b"\xc5\x81agiewniki".decode('utf-8'),
-    b'Rzemie\xc5\x9blnicza'.decode('utf-8')
-])
+def get_new_schedules():
+    try:
+        new_schedules = scraper.get_schedules([
+            'Rondo Matecznego',
+            b"\xc5\x81agiewniki".decode('utf-8'),
+            b'Rzemie\xc5\x9blnicza'.decode('utf-8')
+        ])
+        return new_schedules
+    except Exception as e:
+        print (e)
+        pass # Ignore errors on this thread and go on...
+
+def update_schedules():
+    while (True):
+        new_schedule = get_new_schedules()
+        global schedules
+        schedules = new_schedule
+        time.sleep(60 * 60 * 12)
+
+scraping_thread = threading.Thread(target=update_schedules)
+scraping_thread.daemon = True
+scraping_thread.start()
 
 bottle.run(host='localhost', port=8080, reloader=True)
