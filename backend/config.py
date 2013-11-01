b80f0534681bc672e8138e20b06bf8a1ca2a30ac
Roberto Liffredo <rliffredo@gmail.com>
2013-11-01 14:48:17 +0100

    Tentative for improving compatibility to Python 2 (untested, though)

--- backend/config.py
+++ backend/config.py
@@ -1,29 +1,41 @@
 import sys
-PY2 = sys.version_info[0] == 2
+from os import path
 
+PY2 = sys.version_info[0] == 2
 if PY2:
     from ConfigParser import ConfigParser
 else:
     from configparser import ConfigParser
 
-from os import path
-app_root = path.split(sys.path[0])[0]
-backend_root = path.join(app_root, 'backend')
+# Some patch for python 2 cp65001 bug
+if PY2:
+    import codecs
+    def cp65001(name):
+        if name.lower()=='cp65001':
+            return codecs.lookup('utf-8')
+    codecs.register(cp65001)
 
-config_file = open('stops.ini', 'r', encoding='cp65001')
+def get_defined_stops():
+    app_root = path.split(sys.path[0])[0]
+    backend_root = path.join(app_root, 'backend')
 
-cp = ConfigParser()
-cp.read_file(config_file)
-stops = []
-for section_name in cp.sections():
-    config_data = cp[section_name]
-    stop_data = {}
-    for key in config_data:
-        if key == 'name':
-            stop_data[key] = config_data[key]
-        elif key == 'pane':
-            stop_data[key] = config_data[key]
-        else:
-            stop_data[key] = int(config_data[key])
-    stops.append(stop_data)
+    cp = ConfigParser()
+    with open('stops.ini', 'r', encoding='cp65001') as config_file: # windows utf-8
+        cp.read_file(config_file)
 
+    stops = []
+    for section_name in cp.sections():
+        config_data = cp[section_name]
+        stop_data = {}
+        for key in config_data:
+            if key == 'name':
+                stop_data[key] = config_data[key]
+            elif key == 'pane':
+                stop_data[key] = config_data[key]
+            else:
+                stop_data[key] = int(config_data[key])
+        stops.append(stop_data)
+
+    return stops
+
+stops = get_defined_stops()
